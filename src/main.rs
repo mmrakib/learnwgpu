48070b0601ff933fd377930111409cf7e9ef3e60
Mayaz Rakib <mohammadmrakib@gmail.com>
2024-03-20 20:17:51 +1100

    Added basic winit window management code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{EventLoop, ControlFlow},
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    event_loop.run(move |event, elwt| {
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                println!("Window close requested.");
+                elwt.exit();
+            },
+
+            Event::AboutToWait => {
+                window.request_redraw();
+            },
+
+            Event::WindowEvent {
+                event: WindowEvent::RedrawRequested,
+                ..
+            } => {
+
+            },
+
+            _ => {}
+        }
+    });
 }
